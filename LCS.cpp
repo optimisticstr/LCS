cdc36e856bfcd08b72ee3a90bf110e123a730b42
Abdul Samad <53661372+AbSamDev@users.noreply.github.com>
2020-09-25 22:44:44 +0500

    LCS code with just protos
    Function definitions are still need to be defined☻

--- LCS.cpp
+++ LCS.cpp
@@ -1,1 +1,26 @@
 //here the code begins
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+char b[50][50]; //Global 2D char arr
+
+//Protos
+int LCSLength(char *s1, char *s2);
+void printLCS(char s[], int i, int j);
+
+int main()
+{
+	char s1[50], s2[50];
+	cout<<"Enter 1st string: ";
+	cin >> s1;
+	cout<<"Enter 2nd string: ";
+	cin >> s2;
+	int len = LCSLength(s1, s2);
+    int m = strlen(s1);
+	int n = strlen(s2);
+	cout << "The LCS is ";
+    printLCS(s1, m, n);
+    cout<<" of length "<< len <<endl;
+    return 0;
+}
